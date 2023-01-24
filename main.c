10eedcb340bbf1a4bad7f5001daef00e1f3ffa3f
AlonNegrin <95145508+AlonNegrin@users.noreply.github.com>
2023-01-24 23:04:21 +0200

    function now returns unsigned int instead of int.

--- main.c
+++ main.c
@@ -2,9 +2,9 @@
 
 
 //returns the product of a,b without * operator
-int mult_elements(int a, int b) {
-    int result = 0;
-    int count = 0;
+unsigned int mult_elements(unsigned int a, unsigned int b) {
+    unsigned int result = 0;
+    unsigned int count = 0;
     while (b != 0) {
         if ((b & 1) == 1) {
             result += (a << count);
